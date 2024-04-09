5bedf6d9b61c2bec2767b1ea9563accf2216a9f6
Zaahir Moolla <moollaza@fastmail.fm>
2024-04-09 02:44:04 +0000

    Add NextUI Dep

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -14,6 +16,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
+
 export default config;
