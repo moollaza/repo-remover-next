a0d8f3819bf8b2448cbac390c1425ebacfb040e7
Zaahir Moolla <moollaza@fastmail.fm>
2024-09-07 03:02:35 +0000

    Renmove unused util

--- src/utils.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import axios from "axios";
-
-export const fetcher = (url: string, token: string) =>
-  axios
-    .get(url, { headers: { Authorization: "Bearer " + token } })
-    .then((res) => res.data);
-
-
