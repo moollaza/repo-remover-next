b821c7218b57db09a00839abda1a921176e3f759
Zaahir Moolla <moollaza@fastmail.fm>
2024-09-21 04:01:43 +0000

    Add logout button to dashboard

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,9 +1,35 @@
-import { Divider, Link, User } from "@nextui-org/react";
+import { Button, Divider, Link, Tooltip, User } from "@nextui-org/react";
 
 import useGitHubData from "@hooks/use-github-data";
 
 import { GenerateReposButton } from "@components/generate-repos-button";
 import RepoTable from "@components/repo-table/repo-table";
+
+// Clear the localStorage and redirect to the homepage
+function LogoutButton() {
+  const handleLogout = () => {
+    if (typeof localStorage !== "undefined") {
+      localStorage.clear();
+      window.location.href = "/";
+    }
+  };
+
+  return (
+    <Tooltip
+      content="Click to logout and return to the homepage"
+      placement="bottom"
+    >
+      <Button
+        variant="light"
+        color="danger"
+        onClick={handleLogout}
+        aria-label="Logout Button"
+      >
+        Logout
+      </Button>
+    </Tooltip>
+  );
+}
 
 export default function DashboardPage() {
   const { user, repos, isLoading, isError } = useGitHubData();
@@ -16,7 +42,6 @@ export default function DashboardPage() {
 
       {(isLoading || repos) && (
         <>
-          {/* <p className="text-small text-gray-600">Authenticated as:</p> */}
           <div className="flex items-center">
             <User
               className="mt-2 mb-4"
@@ -35,7 +60,8 @@ export default function DashboardPage() {
                 showFallback: true,
               }}
             />
-            <div className="ml-auto">
+            <div className="flex gap-3 ml-auto">
+              <LogoutButton />
               <GenerateReposButton />
             </div>
           </div>
