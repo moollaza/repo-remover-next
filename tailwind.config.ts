aaa404850dac9e54cc3fc2b6c4462a345a64817b
Zaahir Moolla <moollaza@fastmail.fm>
2024-12-07 00:37:00 -0500

    More sorting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+
 import { nextui } from "@nextui-org/react";
 
 const config: Config = {
@@ -7,17 +8,17 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
+  plugins: [nextui()],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
     },
   },
-  darkMode: "class",
-  plugins: [nextui()],
 };
 
 export default config;
