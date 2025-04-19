476dd45692a5713d14121033f98a8d91320e5e82
Zaahir Moolla <moollaza@fastmail.fm>
2025-04-19 11:26:27 -0400

    Fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,8 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  // Look for test files in the "tests" directory, relative to this configuration file.
-  testDir: "tests",
+  // Look for test files in the "e2e" directory, relative to this configuration file.
+  testDir: "e2e",
 
   // Run all tests in parallel.
   fullyParallel: true,
