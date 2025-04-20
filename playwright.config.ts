a57bee6b73e1c567210492bf74f3cb268310d6c5
Zaahir Moolla <moollaza@fastmail.fm>
2025-04-20 12:41:16 -0400

    Add debug option for e2e tests and adjust retry count in Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
 
   // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
 
   // Opt out of parallel tests on CI.
   workers: process.env.CI ? 1 : undefined,
